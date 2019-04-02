6241a4584dfcc3e9a56fc0f945ab64692519a4c0
GiorgioUghini <giorgio1937@hotmail.it>
2019-04-02 16:41:51 +0200

    Add AmmoCard and WeaponCard to SquareDecorator

--- src/main/java/models/GameMap/SpawnPoint.java
+++ src/main/java/models/GameMap/SpawnPoint.java
@@ -1,8 +1,22 @@
 package models.GameMap;
 
+import models.WeaponCard;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class SpawnPoint extends SquareDecorator {
+
+    private List<WeaponCard> weaponList;
 
     public SpawnPoint(AbstractSquare abstractSquare){
         this.abstractSquare = abstractSquare;
+        this.weaponList = new ArrayList<>();
+    }
+
+    public void spawnWeapon(WeaponCard weaponCard) {
+        if (weaponList.size() < 3) {
+            weaponList.add(weaponCard);
+        }
     }
 }
