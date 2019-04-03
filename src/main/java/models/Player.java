6eb0eac965d651bbd43a624ba2f8031e502e2ad3
GiorgioUghini <giorgio1937@hotmail.it>
2019-04-03 10:56:09 +0200

    Add Player getter and setter and related objects

--- src/main/java/models/Player.java
+++ src/main/java/models/Player.java
@@ -1,25 +1,63 @@
 package models;
 
 import java.util.ArrayList;
-
-class Ammo {
-    public Ammo() {
-        red = 0;
-        green = 0;
-        yellow = 0;
-    }
-    int red;
-    int green;
-    int yellow;
-}
+import java.util.List;
 
 public class Player {
 
+    public enum color {
+        YELLOW,
+        GREY,
+        BLUE,
+        PURPLE,
+        GREEN,
+
+    }
+
     private boolean isFirstPlayer;
     //private Miniatura miniatura;
-    private Ammo ammo = new Ammo();
-    private ArrayList<WeaponCard> weaponList = new ArrayList<>();
-    private ArrayList<PowerUpCard> powerUpList = new ArrayList<>();
+    private color color;
+    private Ammo ammo;
+    private List<WeaponCard> weaponList = new ArrayList<>();
+    private List<PowerUpCard> powerUpList = new ArrayList<>();
 
+    public void setFirstPlayer(boolean firstPlayer) {
+        isFirstPlayer = firstPlayer;
+    }
 
+    public boolean isFirstPlayer() {
+        return isFirstPlayer;
+    }
+
+    public void setColor(Player.color color) {
+        this.color = color;
+    }
+
+    public color getColor() {
+        return color;
+    }
+
+    public void setAmmo(Ammo ammo) {
+        this.ammo = ammo;
+    }
+
+    public Ammo getAmmo() {
+        return ammo;
+    }
+
+    public void setWeaponList(List<WeaponCard> weaponList) {
+        this.weaponList = weaponList;
+    }
+
+    public List<WeaponCard> getWeaponList() {
+        return weaponList;
+    }
+
+    public void setPowerUpList(List<PowerUpCard> powerUpList) {
+        this.powerUpList = powerUpList;
+    }
+
+    public List<PowerUpCard> getPowerUpList() {
+        return powerUpList;
+    }
 }
