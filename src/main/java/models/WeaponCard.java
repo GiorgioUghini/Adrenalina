64ea90d33f25ec74a108243b076baaef29f7b7f5
SimeoneVilardo <simeone.vilardo@gmail.com>
2019-04-06 15:25:16 +0200

    Add cards and decks models

--- src/main/java/models/WeaponCard.java
+++ src/main/java/models/WeaponCard.java
@@ -1,22 +1,4 @@
 package models;
 
-public class WeaponCard extends Card {
-    public enum state {
-        LOADED,
-        PARTIALLY_LOADED,
-        UNLOADED
-    }
-    private state actualState;
-
-    public void setState(state stateToSet) {
-        actualState = stateToSet;
-    }
-
-    public state getState() {
-        return actualState;
-    }
-
-    WeaponCard() {
-        setState(state.PARTIALLY_LOADED);
-    }
+public class WeaponCard {
 }
---
+++ src/main/java/models/card/Effect.java
@@ -0,0 +1,8 @@
+package models.card;
+
+import java.util.List;
+
+public class Effect {
+    public String name;
+    public List<Action> actions;
+}
