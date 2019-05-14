c2976beb4f1d10e2a54d65428dbc303e3e1c28fa
Cosimo Russo <cosimo@menuale.com>
2019-05-14 21:40:17 +0200

    Test ammo points

---
+++ src/test/java/models/map/AmmoPointTest.java
@@ -0,0 +1,56 @@
+package models.map;
+
+import errors.CardAlreadyExistsException;
+import models.card.AmmoCard;
+import models.card.Card;
+import models.card.WeaponCard;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import java.util.UUID;
+
+public class AmmoPointTest {
+    @Test
+    public void testAddCard(){
+        AmmoPoint ammoPoint = new AmmoPoint(RoomColor.YELLOW, 1, UUID.randomUUID());
+        assertNull(ammoPoint.showCard());
+        Card card = new AmmoCard(1,1,1, true);
+        ammoPoint.addCard(card);
+        assertEquals(card, ammoPoint.showCard());
+    }
+    @Test
+    public void testAddCardWithError(){
+        AmmoPoint ammoPoint = new AmmoPoint(RoomColor.YELLOW, 1, UUID.randomUUID());
+        Card card1 = new WeaponCard();
+        Card card2 = new AmmoCard(1,1,1,false);
+        Card card3 = new AmmoCard(2,2,2,true);
+        try{
+            ammoPoint.addCard(card1);
+            assert false;
+        }catch (ClassCastException e){
+            assert true;
+        }
+        try{
+            ammoPoint.addCard(null);
+            assert false;
+        }catch (NullPointerException e){
+            assert true;
+        }
+        try{
+            ammoPoint.addCard(card2);
+            ammoPoint.addCard(card3);
+            assert false;
+        }catch (CardAlreadyExistsException e){assert true;}
+    }
+    @Test
+    public void testDrawCard(){
+        AmmoPoint ammoPoint = new AmmoPoint(RoomColor.YELLOW, 1, UUID.randomUUID());
+        Card card = new AmmoCard(1,1,1, true);
+        ammoPoint.addCard(card);
+        assertEquals(card, ammoPoint.drawCard());
+        try{
+            ammoPoint.drawCard();
+            assert false;
+        }catch(NullPointerException e){assert true;}
+    }
+}
