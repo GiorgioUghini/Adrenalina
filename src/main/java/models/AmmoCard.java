adf8c94b003f71bc3f33652ffe18591765a219ad
Cosimo Russo <cosimo@menuale.com>
2019-05-14 15:30:57 +0200

    Implement AmmoCard model

--- src/main/java/models/AmmoCard.java
+++ src/main/java/models/AmmoCard.java
@@ -1,5 +1,32 @@
 package models;
 
-public class AmmoCard extends Card {
+import errors.InvalidAmmoException;
 
+public class AmmoCard extends Card {
+    private int red;
+    private int blue;
+    private int yellow;
+    private boolean drawPowerup;
+
+    /** Creates the ammo card
+     * @param red number of red ammos to draw, must be between 0 and 3
+     * @param yellow number of red ammos to draw, must be between 0 and 3
+     * @param blue number of red ammos to draw, must be between 0 and 3
+     * @param drawPowerup if true you can draw a powerup from the deck */
+    public AmmoCard(int red, int yellow, int blue, boolean drawPowerup){
+        if(!(validateAmmo(red) && validateAmmo(yellow) && validateAmmo(blue))) throw new InvalidAmmoException();
+        this.red = red;
+        this.blue = blue;
+        this.yellow = yellow;
+        this.drawPowerup = drawPowerup;
+    }
+
+    public int getRed(){return red;}
+    public int getYellow(){return yellow;}
+    public int getBlue(){return blue;}
+    public boolean hasPowerup(){return drawPowerup;}
+
+    private boolean validateAmmo(int n){
+        return (n>=0 && n<=3);
+    }
 }
