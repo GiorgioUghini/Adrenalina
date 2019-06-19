145e63f12d2db5e0757ae58ed7d94f2bbb6aa1e3
SimeoneVilardo <simeone.vilardo@gmail.com>
2019-06-19 17:42:31 +0200

    Add map update in end turn

--- src/main/java/network/RemoteMethods.java
+++ src/main/java/network/RemoteMethods.java
@@ -202,6 +202,7 @@ public class RemoteMethods extends UnicastRemoteObject implements RemoteMethodsI
         Player player = Server.getInstance().getLobby().getPlayer(token);
         Match match = player.getMatch();
         match.nextTurn();
+        match.addUpdate(new MapUpdate(match.getMap()));
         Server.getInstance().getConnection().getConnectionWrapper(token).addUpdate(new PlayerUpdate(player));
         return new EndTurnResponse();
     }
