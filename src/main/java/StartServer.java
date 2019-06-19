f8bd6b3abee78b3715bf940beac4e87a8a5c5da4
Cosimo Russo <cosimo@menuale.com>
2019-06-19 12:02:12 +0200

    Set maximum number of players in a match as a server arg

--- src/main/java/StartServer.java
+++ src/main/java/StartServer.java
@@ -1,10 +1,19 @@
 import network.Server;
+import utils.Constants;
 
 import java.io.IOException;
 
 public class StartServer {
     public static void main(String[] args) throws IOException {
         Server server = Server.getInstance();
+        int maxClients = 5;
+        for (String s: args) {
+            s = s.replace(Constants.ARG_PREFIX, "").toLowerCase();
+            if(s.startsWith("max-clients")){
+                maxClients = Integer.parseInt(s.replace("max-clients=", ""));
+            }
+        }
+        server.setMaxClients(maxClients);
         server.start();
     }
 }
