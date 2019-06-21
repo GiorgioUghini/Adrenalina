bca5be998c3a4162d51f3e788c48b5b7bb7c9958
SimeoneVilardo <simeone.vilardo@gmail.com>
2019-06-21 20:36:54 +0200

    Add auto start for disconnected player

--- src/main/java/network/ClientListener.java
+++ src/main/java/network/ClientListener.java
@@ -1,7 +1,11 @@
 package network;
 
 import models.Match;
+import models.card.PowerUpCard;
+import models.map.GameMap;
+import models.map.SpawnPoint;
 import models.player.Player;
+import network.updates.MapUpdate;
 import network.updates.PlayerDisconnectUpdate;
 import utils.Console;
 
@@ -49,6 +53,17 @@ public class ClientListener implements Runnable{
                     player.disconnect();
                     Match match = Server.getInstance().getLobby().getMatch(player);
                     if(match.getCurrentPlayer().getName().equals(player.getName())){
+                        if(player.hasJustStarted()){
+                            while (player.getPowerUpList().size() < 2){
+                                player.drawPowerUp();
+                            }
+                            PowerUpCard powerUpCard = player.getPowerUpList().get(0);
+                            player.throwPowerUp(powerUpCard);
+                            GameMap map = match.getMap();
+                            SpawnPoint spawnPoint = map.getSpawnPoints().stream().filter(p -> p.getColor() == powerUpCard.color).findFirst().orElse(null);
+                            map.spawnPlayer(player, spawnPoint);
+                            match.addUpdate(new MapUpdate(match.getMap()));
+                        }
                         match.nextTurn();
                     }
                     match.addUpdate(new PlayerDisconnectUpdate(player.getName()));
